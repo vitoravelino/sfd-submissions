1e985d596fa96ce8d0cdedf607056008c44ce8b5
Vítor Avelino <vitor88@gmail.com>
2010-08-06 13:07:47 -0300

    refactoring and modularization

---
+++ app/models.rb
@@ -0,0 +1,26 @@
+require 'dm-core'
+require 'dm-migrations'
+require 'dm-validations'
+
+DataMapper.setup(:default, @db)
+
+class Participant
+  include DataMapper::Resource
+  property :id, Serial
+  property :email, String, :required => true, :unique => true, :format => :email_address
+  property :name, String, :required => true
+  property :present, Boolean, :default => false
+end
+
+class Proposal
+  include DataMapper::Resource
+  property :id, Serial
+  property :author, String, :required => true
+  property :email, String, :required => true, :format => :email_address
+  property :title, String, :required => true
+  property :description, Text, :required => true
+  property :confirmed, Boolean, :default => false
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
--- sfd.rb
+++ sfd.rb
@@ -1,126 +1,71 @@
 require 'rubygems'
 require 'sinatra'
-require 'dm-core'
-require 'dm-migrations'
-require 'dm-validations'
-require 'rack-flash'
+require 'app/config'
+require 'app/models'
+#require 'app/helpers'
 require 'haml'
 require 'sass'
 
-configure do
-	set :sessions, true
-	use Rack::Flash
-end
-
-configure :development do
-	require 'dm-sqlite-adapter'
-	@db = "sqlite::memory:"
-end
-
-configure :production do
-	require 'pony'
-	@db = ENV['DATABASE_URL']
-
-	not_found do
-		haml "We're so sorry, but we don't know what is this."
-	end
-
-	#error do
-#		haml "Something really nasty happened.  We're on it!"
-#	end
-end
-
-DataMapper.setup(:default, @db)
-
-class Participant
-	include DataMapper::Resource
-	property :id, Serial
-	property :email, String, :unique => true, :format => :email_address
-	property :name, String, :required => true
-	property :present, Boolean, :default => false
-end
-
-class Presentation
-	include DataMapper::Resource
-	property :id, Serial
-	property :author, String, :required => true
-	property :email, String, :required => true, :format => :email_address
-	property :title, String, :required => true
-	property :description, Text, :required => true
-	property :confirmed, Boolean, :default => false
-end
-
-DataMapper.finalize
-DataMapper.auto_upgrade!
-
-helpers do
-	def send_mail(email, type)
-		Pony.mail(:to => email, :from => 'sfdcg2010@email.com', :subject => "[SFD2010-CG] #{type} feita com sucesso!", :via => :smtp, :via_options => {
-			:address        => "smtp.sendgrid.net",
-			:port           => "25",
-			:authentication => :plain,
-			:user_name      => ENV['SENDGRID_USERNAME'],
-			:password       => ENV['SENDGRID_PASSWORD'],
-			:domain         => ENV['SENDGRID_DOMAIN'],
-		})
-	end
-end
-
 get '/stylesheet/application.css' do
-	content_type 'text/css', :charset => 'utf-8'
-	sass :stylesheet, :style => :compact
+  content_type 'text/css', :charset => 'utf-8'
+  sass :stylesheet, :style => :compact
 end
 
 get '/' do
   @participants = Participant.all.length
-	@presentations = Presentation.all.length
-	haml :index, :layout => false
+  @proposals = Proposal.all.length
+  haml :index, :layout => false
 end
 
-get '/registration' do
-	haml :registration
+get '/proposal/:id' do |id|
+  @proposal = Proposal.get(id)
+  haml :proposal
 end
 
-post '/registration' do
-	participant = Participant.create(:email => params[:email], :name => params[:name])
-	if participant.save
-		flash[:notice] = "Você está inscrito para participar do SFD 2010 - Campina Grande!"
-		#send_mail(participant.email, "Inscrição")
-		redirect '/'
-	else
-		@errors = participant.errors
-		haml :registration
-	end
+get '/registration/participant' do
+  haml :registration_participant
 end
 
-get '/presentation' do
-	haml :presentation
+post '/registration/participant' do
+  participant = Participant.create(:email => params[:email], :name => params[:name])
+  if participant.save
+    flash[:notice] = "Você está inscrito para participar do SFD 2010 - Campina Grande!"
+    #send_mail(participant.email, "Inscrição")
+    redirect '/'
+  else
+    @errors = participant.errors
+    haml :registration_participant
+  end
 end
 
-post '/presentation' do
-	presentation = Presentation.create(:author => params[:author], :email => params[:email], :title => params[:title], :description => params[:description])
-	if presentation.save
-		flash[:notice] = "Proposta de palestra submetida com sucesso!"
-		#send_mail(presentation.email, "Submissão")
-		redirect '/'
-	else
-		@errors = presentation.errors
-		haml :presentation
-	end
+get '/registration/proposal' do
+  haml :registration_proposal
 end
 
-get '/admin/presentation' do
-	@presentations = Presentation.all(:order => [ :confirmed.desc, :title.asc ])
-	@confirmed = Presentation.all(:confirmed => true).length
-	haml :admin_presentation
+post '/registration/proposal' do
+  proposal = Proposal.create(:author => params[:author], :email => params[:email], :title => params[:title], :description => params[:description])
+  if proposal.save
+    flash[:notice] = "Proposta de palestra submetida com sucesso!"
+    #send_mail(presentation.email, "Submissão")
+    redirect '/'
+  else
+    @errors = proposal.errors
+    haml :registration_proposal
+  end
 end
 
-get '/admin/participant' do
-	@participants = Participant.all(:order => [ :present.desc, :name.asc ])
-	@presents = Participant.all(:present => true).length
-	haml :admin_participant
+get '/admin/proposals' do
+  @proposals = Proposal.all(:order => [ :confirmed.desc, :title.asc ])
+  @confirmed = Proposal.all(:confirmed => true).length
+  haml :admin_proposals
+end
+
+get '/admin/participants' do
+  @participants = Participant.all(:order => [ :present.desc, :name.asc ])
+  @presents = Participant.all(:present => true).length
+  haml :admin_participants
 end
 
 get '/certificate' do
-	haml :certificate
+  haml :certificate
 end
