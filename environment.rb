c054b6bf7ee2c821daa6b4e9c174c39ecbde963c
Vítor Avelino <vitor88@gmail.com>
2010-08-23 21:40:01 -0300

    changing file names

---
+++ environment.rb
@@ -0,0 +1,25 @@
+require 'rack-flash'
+
+configure do
+  set :sessions, true
+  use Rack::Flash
+end
+
+configure :development do
+  require 'dm-sqlite-adapter'
+  @db = "sqlite::memory:"
+end
+
+configure :production do
+  require 'dm-postgres-adapter'
+	@db = ENV['DATABASE_URL']
+
+  not_found do
+    haml "We're so sorry, but we don't know what is this."
+  end
+
+  error do
+    haml "Something really nasty happened.  We're on it!"
+ end
+end
+
